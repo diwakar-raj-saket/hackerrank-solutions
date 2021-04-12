f8a3fb2b7c22d347bf465fb1962015378e4653da
Divyanshu Singh <59568700+divyanshu887@users.noreply.github.com>
2021-04-12 17:42:57 +0530

    Update problem_text wrap.py

--- python-language/problem_text wrap.py
+++ python-language/problem_text wrap.py
@@ -1,7 +1,7 @@
 import textwrap
 
 def wrap(string, max_width):
-    resul=textwrap.fill(string, max_width)
+    result=textwrap.fill(string, max_width)
     return result
 
 if __name__ == '__main__':

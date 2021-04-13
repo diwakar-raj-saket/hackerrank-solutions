8470dfc2bb51b84b53892fb522cf7c74d7ed96ec
Divyanshu Singh <59568700+divyanshu887@users.noreply.github.com>
2021-04-13 08:38:21 +0530

    Create problem_Capitalize!.py

---
+++ python-language/problem_Capitalize!.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    a=s.split(' ')
+    x=''
+    for i in range(len(a)):
+        x=x+a[i].capitalize()+" "
+        
+    return x
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()

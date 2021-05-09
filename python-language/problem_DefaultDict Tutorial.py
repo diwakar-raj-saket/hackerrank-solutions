db1bfa4044293e43ec5d9995032da9b912f191b0
diwakar-raj-saket <76169635+diwakar-raj-saket@users.noreply.github.com>
2021-05-09 11:39:57 +0530

    Update problem_DefaultDict Tutorial.py

--- python-language/problem_DefaultDict Tutorial.py
+++ python-language/problem_DefaultDict Tutorial.py
@@ -1,1 +1,14 @@
-3wip
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict 
+n,m = map(int, input().split())
+d = defaultdict(list)
+for _ in range(1,n+1):
+    d[input()].append(str(_))
+    
+for output in range(0,m): 
+    a = input()
+    if len(d[a]) == 0:
+        print(-1)
+    else:
+        print(' '.join(d[a]))   
+
